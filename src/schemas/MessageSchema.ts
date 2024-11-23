e402016a7cbf6c2d7742dfc6d413cf1e6dcdc311
rahul karakoti <rahulsinghkarakoti28@gmail.com>
2024-11-23 15:34:52 +0530

    "Remove MessageSchema.ts file"

--- src/schemas/MessageSchema.ts
+++
@@ -1,7 +0,0 @@
-import {z} from 'zod'
-
-export const MessageSchema=z.object({
-    content:z.string()
-    .min(5,{message:"content must be more then 5 charecters"})
-    .max(100,{message:"content must not be more then 100 charecters"})
-})
